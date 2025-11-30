af3d658026b985f7629f84ddd159e637cb75d783
root <root@ip-172-31-78-187.ec2.internal>
2025-11-30 11:16:13 +0000

    modited py file

--- app.py
+++ app.py
@@ -8,3 +8,9 @@ if number % 2 == 0:
     print(f"{number} is an even number.")
 else:
     print(f"{number} is an odd number.")
+    def greet(name):
+    """This function greets the person passed in as a parameter."""
+    print(f"Hello, {name}!")
+
+greet("Alice")
+greet("Bob")
