3ddad98e4438de5b752a7dda5a8529062ddd9d91
Naresh Dommata <155455080+nareshdommata@users.noreply.github.com>
2025-11-30 16:20:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ num1 = 10
 num2 = 25
 sum_result = num1 + num2
 print(f"The sum of {num1} and {num2} is: {sum_result}")
+
+number = 7
+if number % 2 == 0:
+    print(f"{number} is an even number.")
+else:
+    print(f"{number} is an odd number.")
